e0930e06696c16798d98aded39aaf48625565e8c
Saurabh Chaudhary <sauchau0@publicisgroupe.net>
2018-12-13 13:11:52 +0530

    initial commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+
+import Person from './Person/Person';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'SA', age: 30 },
+      {name: 'RA', age: 40 },
+      {name: 'TY', age: 50 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi...</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person  name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobiees are: xyz</Person>
+        <Person  name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
