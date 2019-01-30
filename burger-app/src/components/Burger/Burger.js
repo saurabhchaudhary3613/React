79e1aaaedea9104920fbe82fe8c2d612385894b6
saurabhchaudhary3613 <saurabh3613@yahoo.com>
2019-01-30 22:05:48 +0530

    changes

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -3,11 +3,26 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    console.log(props);
+    let tranformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        console.log("igKey", igKey);
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            console.log("_ , i", _, i)
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    })
+    if(tranformedIngredients.length === 0) {
+        tranformedIngredients = <p>Please start add new ingredients..! </p>
+    }
+    console.log('tranformedIngredients', tranformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="chese" />
-            <BurgerIngredient type="meat" />
+            {tranformedIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
